ce15502527dc96fddc2b4391a58a1d7a94dd166d
markaruben <markasebastian@yahoo.com>
2026-01-05 13:50:18 +0200

    initial commit

---
+++ synthed/src/main/java/com/disertatie/synthed/SynthedApplication.java
@@ -0,0 +1,13 @@
+package com.disertatie.synthed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynthedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynthedApplication.class, args);
+	}
+
+}
---
+++ synthed/src/test/java/com/disertatie/synthed/SynthedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disertatie.synthed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynthedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
